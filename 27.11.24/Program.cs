82d92b7d95b6bdc401ab9d362a3d81362ad8be2d
Vova <lololoskafry@gmail.com>
2024-11-27 19:42:59 +0200

    Add project files.

---
+++ 27.11.24/Program.cs
@@ -0,0 +1,83 @@
+﻿
+namespace _27._11._24
+{
+
+    class Play : IDisposable
+    {
+        #region Areas
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int PublicDate { get; set; }
+        #endregion
+
+        #region Constr|Final
+        public Play() { }
+        public Play(string name, string author, string genre, int publicDate)
+        {
+            Name = name;
+            Author = author;
+            Genre = genre;
+            if (publicDate < 0 || publicDate > 2024)
+                throw new Exception("Invalid date");
+            else
+                PublicDate = publicDate;
+        }
+        ~Play()
+        {
+            Console.WriteLine("All areas deleted");
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return "Play: " + Name + "\n" + "Author: " + Author + "\n" + "Genre: " + Genre + "\n" + "PublicDate: " + PublicDate + "\n";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Space cleared");
+        }
+    }
+
+    class Shop : IDisposable
+    {
+        #region Areas
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Type { get; set; }
+        #endregion
+        #region Constr|Disp
+        public Shop() { }
+        public Shop(string name, string adress, string type)
+        {
+            Name = name;
+            Adress = adress;
+            Type = type;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Space clearing");
+        }
+        #endregion
+        ~Shop()
+        {
+            Console.WriteLine("All areas deleted");
+        }
+
+
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
